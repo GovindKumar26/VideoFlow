b4b6729290e898970f960026dc1893bfdb97e81b
Govind Kumar <govindkumar26112001@gmail.com>
2026-07-09 17:59:18 +0530

    1

--- server/worker/index.js
+++ server/worker/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { createReadStream } from 'fs';
-import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 
 import { pipeline } from "node:stream/promises";
 import { createWriteStream } from "node:fs";
